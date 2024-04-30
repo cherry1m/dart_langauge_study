ab9c1aa56b02904efa2605f23aca7b0a59d25212
cherry1m <ini_045@naver.com>
2024-04-30 19:52:29 +0900

    if

---
+++ if.dart
@@ -0,0 +1,36 @@
+void main() {
+  // int a = 10;
+  // int b = 20;
+
+  // if (a > b) {
+  //   print("a가 더 크다.");
+  // }
+  // else if (a == b) {
+  //   print("a랑 b는 같다.");
+  // }
+  // else {
+  //   print("b가 더 크다.");
+  // }
+  // print(a + b);
+
+  int number = 10;
+  switch(number) {
+    case 5:
+      print("5입니다.");
+      break;
+    case 6:
+      print("6입니다.");
+      break;
+    case 7:
+      print("7입니다.");
+      break;
+    case 8:
+      print("8입니다.");
+      break;
+    case 9:
+      print("9입니다.");
+      break;
+      default:
+        print("값이 없습니다.");
+  }
+}
