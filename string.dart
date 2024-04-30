3340335cb396317964b4aabc50a4fdbc77a10fad
cherry1m <ini_045@naver.com>
2024-04-30 19:00:20 +0900

    string

---
+++ string.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "윤채림";
+  String hello = "Hello";
+  print(name);
+  print(name[0]);
+
+  print("${name} ${hello}"); // 두 문자 사이 띄어쓰기 하는 법, 중괄호 생략도 가능
+  print("내 이름은 $name입니다.");
+}
