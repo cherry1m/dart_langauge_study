cd2154613ec430c4ed7d400f34c8426e78ca5096
cherry1m <ini_045@naver.com>
2024-04-30 19:25:51 +0900

    oper

---
+++ oper.dart
@@ -0,0 +1,29 @@
+void main() {
+  int a = 1;
+  int b = 1;
+  // print (a < b);
+
+  // print(a);
+  // a = a + 3;
+  // print(a);
+  // a = a + 1;
+  // print(a);
+  // a++;
+  // print(a);
+
+  // a = a * 3; // a*= 3 과 같은 결과 출력
+  // a += 3;
+  // a = a + 1;
+  // a += 1;
+  // a++;
+
+  // int c = 3;
+  // print(c++);
+  // print(c); // 후위 연산
+
+  int c = 3;
+  print(++c);
+  print(c);
+//   a++;
+//   ++a;
+}
