16d9b8bce84afaf57cf7ad8ccf58c526d7c9020f
cherry1m <ini_045@naver.com>
2024-04-30 18:37:36 +0900

    variable

---
+++ variable.dart
@@ -0,0 +1,19 @@
+void main() {
+  var number = 10;
+  print(number);
+  number = 20;
+  print(number);
+  // number = ("hello world!");
+  // number = 1.1;
+
+  // ------------------------------------------------
+  print("---------------------------------");
+  dynamic number2 = 10;
+  print(number2);
+  number2 = 20;
+  print(number2);
+  number2 = ("hello world!");
+  print(number2);
+  number2 = 1.1;
+  print(number2);
+  }
