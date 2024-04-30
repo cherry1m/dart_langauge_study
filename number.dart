f3c2f7dda5184dd245fdf97d37f89d0c14f681c0
cherry1m <ini_045@naver.com>
2024-04-30 18:46:39 +0900

    number

---
+++ number.dart
@@ -0,0 +1,27 @@
+void main() {
+  // int age = 21;
+  // print(age);
+  // age = 30;
+  // print(age);
+
+  // double pi = 3.14;
+  // print(pi);
+
+  // int a = 10;
+  // int b = 2;
+
+  // print(a + b);
+  // print(a - b);
+  // print(a * b);
+  // print(a / b);
+  // print(a % b);
+
+  double a = 1.1;
+  double b = 0.5;
+
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+}
